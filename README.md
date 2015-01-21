c772a082a1d77802b7d12e7c37d60128412b2a1c
Todd Wolfson <todd@twolfson.com>
2015-01-21 17:08:34 -0600

    Initial release

--- README.md
+++ README.md
@@ -2,19 +2,71 @@
 
 Resolve complete/partial URLs against a canonical target URL
 
+This library was built to resolve URLs when candidates submit incompleted URLs (e.g. missing protocol, username only).
+
+```js
+// Username only
+resolveLink('underdogio', 'https://github.com/');
+  // https://github.com/underdogio
+
+// Missing protocol
+resolveLink('www.linkedin.com/in/toddwolfson', 'https://www.linkedin.com/');
+  // https://www.linkedin.com/in/toddwolfson
+
+// Custom website
+resolveLink('https://underdog.io/', 'https://www.linkedin.com/');
+  // https://underdog.io/
+
+// Complete URL
+resolveLink('https://github.com/underdogio', 'https://github.com/');
+  // https://github.com/underdogio
+```
+
+`resolve-link` is browser compatible and is 6kb when minified and gzipped.
+
 ## Getting Started
+### npm
 Install the module with: `npm install resolve-link`
 
 ```js
 var resolveLink = require('resolve-link');
-resolveLink(); // 'awesome'
+resolveLink('underdogio', 'https://github.com/'); // https://github.com/underdogio
+```
+
+### bower
+Install the module with: `bower install resolve-link`
+
+```html
+<script src="bower_components/resolve-link/dist/resolve-link.min.js"></script>
+<script>
+  window.resolveLink; // Use same as in `npm`
+</script>
+```
+
+### Vanilla
+Download the minified JS at:
+
+https://raw.githubusercontent.com/underdogio/resolve-link/master/dist/resolve-link.min.js
+
+```html
+<script src="resolve-link.min.js"></script>
+<script>
+  window.resolveLink; // Use same as in `npm`
+</script>
 ```
 
 ## Documentation
-_(Coming soon)_
+We expose `resolveLink` as our `module.exports` and on `window.resolveLink` in `bower`/vanilla.
 
-## Examples
-_(Coming soon)_
+### `resolveLink(srcUrl, targetUrl)`
+Resolve the best variation of `srcUrl` with respect to our `targetUrl`
+
+- srcUrl `String` - URL/partial URL to be resolving from
+- targetUrl `String` - Canonical URL to try to match if on the same domain
+
+**Returns:**
+
+- retVal `String` - Completed URL formatted via `node's url.format`
 
 ## Contributing
 In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint via `npm run lint` and test via `npm test`.
--- test/resolve-link-test.js
+++ test/resolve-link-test.js
@@ -1,10 +1,82 @@
 // Load in dependencies
-var assert = require('assert');
+var expect = require('chai').expect;
 var resolveLink = require('../');
 
-// Start our tests
-describe('resolve-link', function () {
-  it('returns awesome', function () {
-    assert.strictEqual(resolveLink(), 'awesome');
+// Define test helpers
+var testUtils = {
+  resolveLink: function (srcUrl, targetUrl) {
+    before(function resolveLinkFn () {
+      this.result = resolveLink(srcUrl, targetUrl);
+    });
+    after(function cleanup () {
+      delete this.result;
+    });
+  }
+};
+
+// Define our test cases
+describe('A complete HTTP URL to our target site when resolved', function () {
+  testUtils.resolveLink('https://www.linkedin.com/in/toddwolfson', 'https://www.linkedin.com/');
+
+  it('points to the original URL', function () {
+    expect(this.result).to.equal('https://www.linkedin.com/in/toddwolfson');
+  });
+});
+
+describe('An HTTP URL without a protocol to our target site when resolved', function () {
+  testUtils.resolveLink('www.linkedin.com/in/toddwolfson', 'https://www.linkedin.com/');
+
+  it('points to the original URL with a protocol', function () {
+    expect(this.result).to.equal('https://www.linkedin.com/in/toddwolfson');
+  });
+});
+
+describe('An HTTP URL with an unexpected path to our target site when resolved', function () {
+  testUtils.resolveLink('https://www.linkedin.com/pub/toddwolfson/aa/bb/cc', 'https://www.linkedin.com/');
+
+  it('points to the original URL', function () {
+    expect(this.result).to.equal('https://www.linkedin.com/pub/toddwolfson/aa/bb/cc');
+  });
+});
+
+describe('An HTTP URL with a query string to our target site when resolved', function () {
+  testUtils.resolveLink('https://www.linkedin.com/profile/view?id=87904336&trk=nav_responsive_tab_profile_pic',
+    'https://www.linkedin.com/');
+
+  it('points to the original URL', function () {
+    expect(this.result).to.equal(
+      'https://www.linkedin.com/profile/view?id=87904336&trk=nav_responsive_tab_profile_pic');
+  });
+});
+
+describe('An HTTP URL to a custom site when resolved', function () {
+  testUtils.resolveLink('http://underdog.io/', 'https://www.linkedin.com/');
+
+  it('points to the original URL', function () {
+    expect(this.result).to.equal('http://underdog.io/');
+  });
+});
+
+describe('An HTTP URL without a pathname to a custom site when resolved', function () {
+  testUtils.resolveLink('http://underdog.io', 'https://www.linkedin.com/');
+
+  it('points to the original URL with a pathname', function () {
+    expect(this.result).to.equal('http://underdog.io/');
+  });
+});
+
+describe('An HTTP URL without a protocol to a custom site when resolved', function () {
+  testUtils.resolveLink('underdog.io', 'https://www.linkedin.com/');
+
+  it('points to the original URL with a protocol and pathname', function () {
+    expect(this.result).to.equal('http://underdog.io/');
+  });
+});
+
+describe('A username to our target site when resolved', function () {
+  testUtils.resolveLink('underdogio', 'https://github.com/');
+
+  it('points to the username on the target site', function () {
+    expect(this.result).to.equal('https://github.com/underdogio');
   });
 });
